dd718ea216c5499ca5173c4fde838eef1c5c4607
Syntax Error <63340001+Alpha-404@users.noreply.github.com>
2021-07-04 18:44:07 +0100

    Update IRL time.lua

--- IRL time.lua
+++ IRL time.lua
@@ -9,5 +9,6 @@
 --no longer burn ur eyes
 --the time syncs with the outside time
 
+game:GetService("Lighting").GeographicLatitude = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://restcountries.eu/rest/v2/alpha/" .. game:GetService("LocalizationService"):GetCountryRegionForPlayerAsync(game:GetService("Players").LocalPlayer))).latlng[1]
 for i,v in pairs(game:GetService("Lighting"):GetChildren()) do if v:IsA("Sky") then v:Destroy() end end
 while wait(1) do game:GetService("Lighting").TimeOfDay = os.date("%H") .. ":" .. os.date("%M") .. ":" .. os.date("%S") end
